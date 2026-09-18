0bc06d14666e1932ef8ce75e333380cc6b01a2ff
Willy Liu(HQ ENG) <willyliu@17.media>
2026-09-18 15:52:36 +0800

    feat(logs): add Firebase Crashlytics log delivery

--- workflows/query_steam_logs_v3_QueryLogsV3A0001/nodes/FirebaseLog/sqlQuery.sql
+++ workflows/query_steam_logs_v3_QueryLogsV3A0001/nodes/FirebaseLog/sqlQuery.sql
@@ -22,7 +22,21 @@ SELECT
   user,
   custom_keys,
   process_state
-FROM `media-b6ace.firebase_crashlytics.com_machipopo_media17_ANDROID_REALTIME`
+FROM `{{
+  (() => {
+    const platform = String($json.type ?? '').toUpperCase();
+
+    if (platform === 'ANDROID') {
+      return 'media-b6ace.firebase_crashlytics.com_machipopo_media17_ANDROID_REALTIME';
+    }
+
+    if (platform === 'IOS') {
+      return 'media17-prod.firebase_crashlytics.com_machipopo_story17_IOS_REALTIME';
+    }
+
+    throw new Error(`Unsupported platform: ${$json.type}`);
+  })()
+}}`
 WHERE event_timestamp BETWEEN startTime AND endTime
   AND user.id = "{{ $json.userID }}"
   AND error_type IN UNNEST(target_error_type)
